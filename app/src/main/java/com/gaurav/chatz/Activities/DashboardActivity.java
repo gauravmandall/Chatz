0d91889d37a3261b8c86de566c6a5220f09daffd
Indian-Hacker <72154932+Indian-hacker@users.noreply.github.com>
2021-07-24 19:55:15 +0530

    Initial commit

---
+++ app/src/main/java/com/gaurav/chatz/Activities/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.gaurav.chatz.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gaurav.chatz.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
